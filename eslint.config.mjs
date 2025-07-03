44b5f7dd643ca6666cc9c9a54034aa18dd8bb27e
Bergkley <bergkley@gmail.com>
2025-07-02 23:23:56 -0300

    feat: adicionar configurações de formatação, incluindo .editorconfig, .prettierrc e eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,43 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-]);
+export default tseslint.config(
+  // Configuração para JavaScript e TypeScript
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+  },
+  // Configurações adicionais, se necessário
+  {
+    rules: {
+      semi: ["error", "always"],
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          ts: "never",
+        },
+      ],
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": ["error"],
+      "@typescript-eslint/no-unused-vars": [2, { args: "none" }],
+      "import/prefer-default-export": "off",
+      "class-methods-use-this": "off",
+      "max-classes-per-file": "off",
+      "no-array-constructor": "off",
+      "no-useless-constructor": "off",
+      "no-empty-function": "off",
+      "no-underscore-dangle": "off",
+    },
+  }
+);
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,22 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import env from 'env-var';
-import router from './routes';
-import databaseHelper from '@/loaders/database';
-
+import dotenv from "dotenv";
+import express from "express";
+import env from "env-var";
+import router from "./routes";
+import databaseHelper from "@/loaders/database";
 
 const app = express();
 dotenv.config();
 
-
-const port = env.get('PORT').required().asPortNumber();
-const NODE_ENV = env.get('NODE_ENV').default('development').asString();
+const port = env.get("PORT").required().asPortNumber();
+const NODE_ENV = env.get("NODE_ENV").default("development").asString();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Iniciar conexões com o banco de dados
-databaseHelper.initConnections()
+databaseHelper.initConnections();
 
-app.use('/api', router);
+app.use("/api", router);
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port} - Ambiente: ${NODE_ENV}`);
 });
